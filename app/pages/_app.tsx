94dd798caf6e7d1f46d0d42112bc65aef2e1cdb2
Tommy Long Pham <tpham@smugmug.com>
2022-03-05 22:16:02 -0800

    Add apollo client and clean up

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'; 
+import type { AppProps } from 'next/app';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
-export default MyApp
+export default App;
